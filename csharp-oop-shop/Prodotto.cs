dead1d430fa38db342294fd768067707490caf3e
IIODERII <oderrisi@gmail.com>
2024-04-19 15:40:29 +0200

    completed

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        public string Nome {  get; set; } 
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public int Iva { get; set; }
+        public int Codice { get; }
+
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+            Random numeroRandom = new Random();
+            this.Codice = numeroRandom.Next(1, 100000000);
+        }
+
+        public double PrezzoEIva() {
+            return Prezzo * (Iva + 100)/100;
+        }
+
+        public string NomeCompleto()
+        {
+            return Convert.ToString(this.Codice) + this.Nome;
+        }
+
+        public string CodiceConPadLeft()
+        {
+            return Convert.ToString(this.Codice).PadLeft(8, '0');
+        }
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace csharp_oop_shop
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_oop_shop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Prodotto[] prodotti =
+            {
+                new Prodotto("Maglietta", "E' una bella maglietta bianca", 12.5, 22),
+                new Prodotto("Pantaloni", "Sono dei bei pantaloni blu", 35.70, 10),
+                new Prodotto("Camicia", "E' una bella camicia verde", 100, 22),
+                new Prodotto("Cappello", "E' un bel cappello nero", 5.50, 4),
+                new Prodotto("Cravatta", "E' una bella cravatta rossa", 14.60, 22),
+            };
+
+            for(int i = 0; i < prodotti.Length; i++)
+            {
+                Console.WriteLine($"{i+1}.");
+                Console.WriteLine($"Nome Prodotto: {prodotti[i].Nome}");
+                Console.WriteLine($"Descrizione Prodotto: {prodotti[i].Descrizione}");
+                Console.WriteLine($"Prezzo senza IVA: EUR {string.Format("{0:C}", prodotti[i].Prezzo)}");
+                Console.WriteLine($"IVA prodotto: {prodotti[i].Iva}%");
+                Console.WriteLine($"Prezzo con IVA: EUR {string.Format("{0:C}", prodotti[i].PrezzoEIva())}");
+                Console.WriteLine($"Codice Prodotto: {prodotti[i].CodiceConPadLeft()}");
+                Console.WriteLine($"Nome completo del prodotto: {prodotti[i].NomeCompleto()}");
+                Console.WriteLine("\n");
+            }
         }
     }
 }
